6a2d4197fe8ab572f921722a2114834526a0c2a9
ujw0704 <ujwalsharma571@gmail.com>
2024-01-04 23:14:27 +0530

    changes

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -18,20 +18,21 @@ function Header() {
 
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
 
-        <Navbar.Collapse id="basic-navbar-nav" className='navcollaps'>
+        <Navbar.Collapse id="basic-navbar-nav">
           <Nav  className='navs' >
             
-            <Nav.Link as={Link} to="/">Home</Nav.Link>
-            <Nav.Link as={Link} to="/Login">Login</Nav.Link>
+            <Nav.Link as={Link} to="/"className='navlink'>Home</Nav.Link>
+            <Nav.Link as={Link} to="/Login"className='navlink'>Login</Nav.Link>
 
-            <NavDropdown title="Search" id="basic-nav-dropdown">
-              <NavDropdown.Item as={Link} to="/Searchjob">Searchjob</NavDropdown.Item>
+            {/* <NavDropdown title="Search" className='navlink'id="basic-nav-dropdown"> */}
+            <NavDropdown title={<span className="custom-dropdown-title">Search</span>} id="basic-nav-dropdown">
+              <NavDropdown.Item as={Link} to="/Searchjob" className='navlink'>Searchjob</NavDropdown.Item>
              <NavDropdown.Divider />
-              <NavDropdown.Item as={Link} to="/Searchjob">Searchjob</NavDropdown.Item>
+              <NavDropdown.Item as={Link} to="/Searchjob" className='navlink'>Searchjob</NavDropdown.Item>
             </NavDropdown>
 
-            <Nav.Link as={Link} to="/about">About-us</Nav.Link>
-            <Nav.Link as={Link} to="/contact">Contact-us</Nav.Link>
+            <Nav.Link as={Link} to="/about"className='navlink'>About-us</Nav.Link>
+            <Nav.Link as={Link} to="/contact"className='navlink'>Contact-us</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Navbar>
